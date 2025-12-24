52378e6a6c0106bf7e76161d5d9ad23cbf3f7796
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-12-23 20:32:04 -0500

    Add ShaderGlobalsCategory class

---
+++ game/addons/tools/Code/Editor/ProjectSettings/ShaderGlobalsCategory.cs
@@ -0,0 +1,19 @@
+﻿namespace Editor.ProjectSettingPages;
+
+[Title( "Shader Globals" )]
+internal sealed class ShaderGlobalsCategory : ProjectInspector.Category
+{
+
+	public override void OnInit( Project project )
+	{
+		base.OnInit( project );
+
+
+	}
+
+	public override void OnSave()
+	{
+		base.OnSave();
+	}
+
+}
