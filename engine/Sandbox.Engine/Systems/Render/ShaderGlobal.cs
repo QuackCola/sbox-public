4bafd25fec8b203208e93445f43971e5da110684
QuackCola <23345567+QuackCola@users.noreply.github.com>
2025-12-23 22:09:19 -0500

    Add ShaderGlobal class

---
+++ engine/Sandbox.Engine/Systems/Render/ShaderGlobal.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Sandbox;
+
+[Expose]
+public class ShaderGlobal
+{
+	public string Name { get; set; }
+
+	public object DefaultValue { get; set; }
+
+	public ShaderGlobal( string name, object defaultValue )
+	{
+		Name = name;
+		DefaultValue = defaultValue;
+	}
+
+	public ShaderGlobal()
+	{
+	}
+}
